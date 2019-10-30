2eb64f4e1acd612163ed36df48bd26a326209168
Si Liang Lei <slei01@stuy.edu>
2019-10-30 11:18:30 -0400

    add functions

--- linkedSongs.c
+++ linkedSongs.c
@@ -1,6 +1,12 @@
-struct node * insert_front(struct node *pointer, int number){
+#include <stdio.h>
+#include <stdlib.h>
+#include "linkedSongs.h"
+#include <time.h>
+
+struct node * insert_front(struct node *pointer, char *name, char *artist){
   struct node *begin = malloc(sizeof(struct node));
-  begin -> i = number;
+  begin -> name = name;
+  begin -> artist = artist;
   begin -> next = pointer;
   return begin;
 }
@@ -12,7 +18,7 @@ void print_list(struct node *pointer) {
   else {
     printf("[");
     while(pointer != NULL){
-      printf("%d", pointer->i);
+      printf("'%s' by %s", pointer->name, pointer->artist);
       if (pointer -> next != NULL){
         printf(", ");
       }
@@ -33,12 +39,28 @@ struct node * free_list(struct node *pointer) {
   return pointer;
 }
 
-struct node * remove_node(struct node *front, int data){
+struct node * randomNode(struct node *pointer){
+  int size;
+  struct node *temp = pointer;
+  while(temp != NULL){
+    temp = temp -> next;
+    size++;
+  }
+  srand(time(0));
+  a = rand() % size;
+  while(a > 0){
+    pointer = pointer->next;
+    a--;
+  }
+  return pointer;
+}
+
+struct node * remove_node(struct node *front, char *name, char *artist){
   if(front == NULL) return front;
   struct node * slider = front;
   struct node * slider_before = NULL;
   while(slider != NULL){
-    if(slider->i == data){
+    if(slider->name == name && slider-> artist == artist){
       struct node * temp = slider;
       if(slider_before == NULL) front = front -> next;
       else slider_before->next = temp->next;
---
+++ main.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "musicLibrary.h"
+#include "linkedSongs.h"
